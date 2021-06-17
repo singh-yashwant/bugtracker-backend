e7633502f8d76a95cee86c4fce3c3b4847e4b6e1
Yashwant Singh <yashwantsinghbtp@gmail.com>
2021-06-17 14:46:47 +0530

    added the team registeration api

--- app.py
+++ app.py
@@ -1,58 +1,71 @@
-import flask
-from flask import request
-from pymongo import MongoClient
-from bson.json_util import dumps
-from bson.objectid import ObjectId 
-from flask import jsonify, request
+from flask import Flask
+from flask_restful import Api
+from registerTeam import RegisterTeam
 
+app = Flask(__name__, static_url_path="")
 
-app = flask.Flask(__name__)
+api = Api(app)
 
-client = MongoClient("mongodb+srv://yashwant:yashwant@cluster0.jibtk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.get_database("bugtracker_db")
+api.add_resource(RegisterTeam, "/register-team")
 
 @app.route('/', methods=['GET'])
 def home():
     return "api working"
 
+
+# import flask
+# from flask import request
+# from pymongo import MongoClient
+# from bson.json_util import dumps
+# from bson.objectid import ObjectId 
+# from flask import jsonify, request
+# from config import MONGO_CONNECTION_URL
+
+
+# app = flask.Flask(__name__)
+
+# client = MongoClient(MONGO_CONNECTION_URL)
+# db = client.get_database("bugtracker_db")
+
+
 # add a new user
-@app.route('/add-user', methods=['POST'])
-def add_user():
-    _json = request.json
-    _name = _json['name']
-    _email = _json['email']
+# @app.route('/add-user', methods=['POST'])
+# def add_user():
+#     _json = request.json
+#     _name = _json['name']
+#     _email = _json['email']
 
-    if _name and _email and request.method == 'POST':
-        db.users.insert({'name': _name, 'email': _email})
+#     if _name and _email and request.method == 'POST':
+#         db.users.insert({'name': _name, 'email': _email})
 
-        resp = jsonify("User added successfully")
-        resp.status_code = 200
-        return resp
+#         resp = jsonify("User added successfully")
+#         resp.status_code = 200
+#         return resp
     
-    else:
-        return not_found()
+#     else:
+#         return not_found()
 
-@app.route('/users', methods=['GET'])
-def users():
-    users = db.users.find()
-    resp = dumps(users)
-    return resp
+# @app.route('/users', methods=['GET'])
+# def users():
+#     users = db.users.find()
+#     resp = dumps(users)
+#     return resp
 
-@app.route('/user/<id>', methods=['GET'])
-def user(id):
-    user = db.users.find_one({"_id":ObjectId(id)})
-    resp = dumps(user)
-    return resp
+# @app.route('/user/<id>', methods=['GET'])
+# def user(id):
+#     user = db.users.find_one({"_id":ObjectId(id)})
+#     resp = dumps(user)
+#     return resp
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': "not found" + request.url
-    }
-    resp = jsonify(message)
-    resp.status_code = 404
-    return resp
+# @app.errorhandler(404)
+# def not_found(error=None):
+#     message = {
+#         'status': 404,
+#         'message': "not found" + request.url
+#     }
+#     resp = jsonify(message)
+#     resp.status_code = 404
+#     return resp
 
 # APIs to implement
 # -register team
---
+++ registerTeam.py
@@ -0,0 +1,60 @@
+from pymongo import MongoClient
+from config import MONGO_CONNECTION_URL
+from bson.objectid import ObjectId
+from flask_restful import Resource
+from flask import request
+import json
+
+"""
+
+register-team json pyload format
+{"team-name": "...", "team-members": [[name1, email1], [name2, email2], ....] ,"password": "..."}
+
+"""
+
+class RegisterTeam(Resource):
+    def __init__(self):
+        self.client = MongoClient(MONGO_CONNECTION_URL)
+        self.db = self.client.bugtracker_db
+        self.user_collection = self.db.users
+        self.team_collection = self.db.teams
+        self.result = {"Status": True, "data": {}}
+
+    def post(self):
+        reqData = json.loads(request.data) if request.data else None
+
+        # print(reqData)
+        # return {"Status": False, "data": "Failed to register"}, 400 
+               
+        # if team with same name already present
+        teamName = reqData["team-name"]
+        # print(teamName)
+        if self.sameNameTeamExist(teamName):
+            self.result["data"] = "Cannot register team with same name already exist"
+            return self.result, 400
+        
+        self.createTeam(reqData)
+        self.addTeamMembersToDb(reqData["team-members"], teamName)
+        self.result["data"] = "Team added successfully"
+        return self.result, 200
+
+    def createTeam(self, teamDetails):
+        self.team_collection.insert(teamDetails)
+        print("team added successfully")
+    
+    def sameNameTeamExist(self, teamName):
+        if self.getTeam(teamName):
+            return True
+        return False
+
+    def getTeam(self, teamName):
+        return self.team_collection.find_one({"team-name": teamName})
+
+    def addTeamMembersToDb(self, members, teamName):
+        for member in members:
+            user = {
+                "name": member[0], 
+                "email": member[1], 
+                "team": teamName
+            }
+            self.user_collection.save(user)
